2543420b44ca54a77bb28e76754f79f1d92a9ef0
Liz Daly <liz7daly@gmail.com>
2020-01-07 19:46:03 +0000

    code passes test: year divisible by 4 and not 100

--- spec/leap_years_spec.rb
+++ spec/leap_years_spec.rb
@@ -1,8 +1,8 @@
 require 'leap_years.rb'
 
-describe 'leap_years' do
+describe 'A leap year' do
 
-  context 'year is divisible by 400' do
+  context 'is divisible by 400:' do
 
     it 'returns true if passed 400' do
       expect(leap_year(400)).to eq true
@@ -18,5 +18,20 @@ describe 'leap_years' do
 
   end
 
+  context 'is divisible by 4 and not by 100:' do
+
+    it 'returns true if passed 2004' do
+      expect(leap_year(2004)).to eq true
+    end
+
+    it 'returns true if passed 2008' do
+      expect(leap_year(2004)).to eq true
+    end
+
+    it 'returns false if passed 100' do
+      expect(leap_year(100)).to eq false
+    end
+
+  end
 
 end
